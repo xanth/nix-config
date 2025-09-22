3e9c6605c643496c73dd6615c6a4c33c78bc39d2
Rhys Williams <5460583+xanth@users.noreply.github.com>
2025-09-22 22:08:52 +1000

    refactor: remove persistent app entries from macOS dock configuration

--- modules/system-dock.nix
+++ modules/system-dock.nix
@@ -13,23 +13,5 @@
     show-process-indicators = true; # show dots under running applications
     orientation = "bottom"; # dock position on screen
     mru-spaces = false; # disable most recently used spaces reordering
-    
-    # Dock app ordering and layout
-    persistent-apps = [
-      "/Applications/Safari.app"
-      "/Applications/Yubico Authenticator.app"
-      "/Applications/Nix Apps/Rider.app"
-      "/Applications/Nix Apps/Visual Studio Code.app"
-      "/Applications/Nix Apps/Alacritty.app"
-      "/Applications/Podman Desktop.app"
-      "/System/Applications/Messages.app"
-      "/System/Applications/Mail.app"
-      "/System/Applications/Calendar.app"
-      "/System/Applications/FaceTime.app"
-      "/System/Applications/Contacts.app"
-      "/Users/${hostConfig.userName}/Applications/YouTube Music.app"
-      "/Users/${hostConfig.userName}/Applications/NetBank.app"
-      "/System/Applications/Launchpad.app"
-    ];
   };
 }
