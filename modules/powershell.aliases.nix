65ff7c055558a80e981d549d12d7ad1223e1500e
Rhys Williams <5460583+xanth@users.noreply.github.com>
2025-11-02 01:50:02 +1000

    feat: add common file system aliases to PowerShell configuration

--- modules/powershell.aliases.nix
+++ modules/powershell.aliases.nix
@@ -4,4 +4,11 @@
 Set-Alias -Name clip -Value Set-Clipboard
 Set-Alias -Name pbcopy -Value Set-Clipboard
 Set-Alias -Name pbpaste -Value Get-Clipboard
+
+# File system
+Set-Alias -Name ls -Value Get-ChildItem
+Set-Alias -Name cp -Value Copy-Item
+Set-Alias -Name mv -Value Move-Item
+Set-Alias -Name rm -Value Remove-Item
+Set-Alias -Name mkdir -Value New-Item
 ''
